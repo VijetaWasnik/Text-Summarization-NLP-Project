b2d15fc9829709c4c5c7b727f666dbe38213b324
VijetaWasnik <vijetawasnik200@gmail.com>
2024-01-04 14:25:14 +0530

    Data Validation completed

---
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+import os
+from textSummarizer.logging import logger
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.conponents.data_validation import DataValidation, DataValidationConfig
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
